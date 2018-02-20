047f6d215f90cd915306cacc7f4d387b0bfa76b1
Unknown <jonahgaynor@gmail.com>
2018-02-20 10:16:53 -0500

    New sprite switching script (needs sprites)

---
+++ Assets/Scripts/SpriteSwitchScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpriteSwitchScript : MonoBehaviour {
+
+	public SpriteRenderer myRenderer;
+	public Sprite[] allSprites;
+	Scene thisScene;
+	string sceneName;
+
+	// Use this for initialization
+	void Start () {
+		thisScene = SceneManager.GetActiveScene();
+		sceneName = thisScene.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		thisScene = SceneManager.GetActiveScene();
+		sceneName = thisScene.name;
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (sceneName == "ChildhoodScene") {
+			if (col.gameObject.name == "Outside") {
+				myRenderer.sprite = allSprites [1];
+			}
+			if (col.gameObject.name == "Inside") {
+				myRenderer.sprite = allSprites [2];
+			}
+			if (col.gameObject.name == "TV") {
+				myRenderer.sprite = allSprites [3];
+			}
+			if (col.gameObject.name == "Bed") {
+				myRenderer.sprite = allSprites [4];
+			}
+		}
+		if (sceneName == "ChildChoiceScene") {
+			if (col.gameObject.name == "Mutate") {
+				myRenderer.sprite = allSprites [1];
+			}
+		}
+		if (sceneName == "SportsScene") {
+			if (col.gameObject.name == "Steroids") {
+				myRenderer.sprite = allSprites [1];
+			}
+			if (col.gameObject.name == "NewTeam") {
+				myRenderer.sprite = allSprites [2];
+			}
+			if (col.gameObject.name == "Retire1") {
+				myRenderer.sprite = allSprites [3];
+			}
+			if (col.gameObject.name == "Retire2") {
+				myRenderer.sprite = allSprites [4];
+			}
+			if (col.gameObject.name == "Retire3") {
+				myRenderer.sprite = allSprites [5];
+			}
+		}
+	}
+}
