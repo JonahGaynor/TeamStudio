50b095a830e4e4cc45d0333245bbd00c0435ce1f
Unknown <jonahgaynor@gmail.com>
2018-02-26 15:35:19 -0500

    Auto stash before merge of "Jonah" and "origin/master"

---
+++ Assets/Scripts/LoveMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoveMovement : MonoBehaviour {
+
+	public float jumpSpeed;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow)) {
+			rb.AddForce (0f, jumpSpeed);
+		}
+	}
+}
