01ea30d0695c12450b7f5f9bee1ca7787f7fcea3
Unknown <jonahgaynor@gmail.com>
2018-02-26 21:21:54 -0500

    Most everything done. 2 issues we can definitely easily solve

--- Assets/Scripts/LoveMovement.cs
+++ Assets/Scripts/LoveMovement.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class LoveMovement : MonoBehaviour {
 
-	public float jumpSpeed;
+	float jumpForce;
 	public Rigidbody2D rb;
+	float runSpeed = 0.2f;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
+		rb = this.GetComponent<Rigidbody2D> ();
+		jumpForce = 2f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow)) {
-			rb.AddForce (0f, jumpSpeed);
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+			rb.AddForce (transform.up * jumpForce);
+			Debug.Log (jumpForce);
+			Debug.Log ("Let's jump!");
 		}
+		runSpeed = SixLaneGameController.Instance.standardMoveSpeed;
+		Vector3 temp = this.transform.position;
+		temp.x += runSpeed;
+		this.transform.position = temp;
 	}
 }
