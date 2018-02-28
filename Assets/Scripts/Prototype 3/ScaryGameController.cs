93bc20610c0e36f03317b694e4a8e9a821c9def7
Unknown <sap565@nyu.edu>
2018-02-28 10:35:20 -0500

    Spooky Scene Fixes

--- Assets/Scripts/Prototype 3/ScaryGameController.cs
+++ Assets/Scripts/Prototype 3/ScaryGameController.cs
@@ -8,24 +8,34 @@ public class ScaryGameController : MonoBehaviour {
 
     public static ScaryGameController Instance = new ScaryGameController();
     public int life = 10;
-    public int numPickups;
+    int prevPickups;
+    public int numPickups=0;
     public float standardMoveSpeed = 0.175f;
     public bool gameOver = false;
     public float timeToWait = 2;
     public GameObject player;
+   // CharacterControlSixLane charScript;
 
     // Use this for initialization
     void Start()
     {
            life = 10;
+        prevPickups = numPickups;
     Instance = this;
-        standardMoveSpeed = 0.075f;
+        standardMoveSpeed = 0.1f;
         // standardMoveSpeed = 5;
+       // charScript = player.GetComponent<CharacterControlSixLane>();
     }
 
     // Update is called once per frame
     void Update()
     {
+      //  charScript.runSpeed = standardMoveSpeed;
+        if (prevPickups != numPickups)
+        {
+            life++;
+                prevPickups = numPickups;
+        }
       
       
         
