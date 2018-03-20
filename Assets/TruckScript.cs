93c82d9884aa8e7d7da58b5dd39d479bccf09551
Unknown <sap565@nyu.edu>
2018-03-20 01:36:30 -0400

    Truck Moves Now, and Stops When He Hits Dude

---
+++ Assets/TruckScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckScript : MonoBehaviour {
+    float speed = 0.01f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 temp = this.transform.position;
+        temp.x -= speed;
+        this.transform.position = temp;
+	}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Little Boy")
+        {
+            speed = 0;
+        }
+    }
+}
