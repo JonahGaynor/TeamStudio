527c8b1d67d207ec00b18b09cbbc1ec55762ae5b
Unknown <sap565@nyu.edu>
2018-03-22 14:41:09 -0400

    RunScene Small Fixes

--- Assets/ALLPLAYTESTSTUFF/Scripts/KillScript.cs
+++ Assets/ALLPLAYTESTSTUFF/Scripts/KillScript.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KillScript : MonoBehaviour {
-
+    GameObject player;
 	// Use this for initialization
 	void Start () {
-		
+        player = GameObject.Find("Little Boy");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 temp = this.transform.position;
+        temp.x = player.transform.position.x - 30;
+        this.transform.position = temp;
 	}
     void OnTriggerEnter2D(Collider2D collider)
     {
