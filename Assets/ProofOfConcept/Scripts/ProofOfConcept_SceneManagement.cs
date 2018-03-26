f9a33dfed7d274191f97828213c5e000aa38c906
Unknown <jonahgaynor@gmail.com>
2018-03-26 13:01:08 -0400

    is new zealand real?

--- Assets/ProofOfConcept/Scripts/ProofOfConcept_SceneManagement.cs
+++ Assets/ProofOfConcept/Scripts/ProofOfConcept_SceneManagement.cs
@@ -27,6 +27,7 @@ public class ProofOfConcept_SceneManagement : MonoBehaviour {
 
 		if (SixLaneGameController.Instance.moveToNextLevel) {
 			if (sceneName == "ProofStartScene") {
+				Debug.Log ("gotta blast!");
 				SceneManager.LoadScene ("ProofEKG");
 			} else if (sceneName == "ProofEKG") {
 				SceneManager.LoadScene ("ProofChildhood");
