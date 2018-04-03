5035e7b56aa4ea438154fadec4c542405811d445
Unknown <sap565@nyu.edu>
2018-04-03 16:17:48 -0400

    I Feel Like It

---
+++ Assets/TestSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TestSceneManager : MonoBehaviour {
+    string sceneName;
+    Scene thisScene;
+	// Use this for initialization
+	void Start () {
+        thisScene = SceneManager.GetActiveScene();
+        sceneName = thisScene.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(sceneName);
+
+        }
+	}
+}
