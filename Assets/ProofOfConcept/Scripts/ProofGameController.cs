24dff048509bef0b731d1a9490cfe7515ae82117
Unknown <sap565@nyu.edu>
2018-04-19 15:01:53 -0400

    GameController Fixes

--- Assets/ProofOfConcept/Scripts/ProofGameController.cs
+++ Assets/ProofOfConcept/Scripts/ProofGameController.cs
@@ -4,27 +4,18 @@ using UnityEngine;
 
 public class ProofGameController : MonoBehaviour {
 
-
-
 	public static ProofGameController Instance = new ProofGameController();
     public int life = 10;
-    public bool startQuestion = false;
     public float standardMoveSpeed = .18f;
     public bool gameOver = false;
     public float timeToWait = 2;
     public bool fadeToEnd = false;
     public float timeToEnd = 5;
-    public bool topChoiceMade = false;
-    public bool bottomChoiceMade = false;
     public bool moveToNextLevel = false;
     public int levelsPast = 0;
-    public int questionsAnswered;
     public bool stopSpawning = false;
     public bool spawnFadePrefab = false;
 	public bool weWon = false;
-    public float timeTillNextQuestion;
-    int currentQuestion = 3;
-    public GameObject[] questionPrompt;
     public GameObject player;
     int prevPickups;
     public int numPickups = 0;
@@ -40,11 +31,6 @@ public class ProofGameController : MonoBehaviour {
         deathSound = this.GetComponent<AudioSource>();
         life = 3;
    		Instance = this;
-        //standardMoveSpeed = 0.0005f;
-		questionsAnswered = 2;
-		timeTillNextQuestion = 5f;
-
-        // standardMoveSpeed = 5;
     }
 
     // Update is called once per frame
@@ -54,38 +40,12 @@ public class ProofGameController : MonoBehaviour {
         {
             gameOver = true;
         }
-        if (prevPickups != numPickups)
-        {
-            life++;
-            prevPickups = numPickups;
-        }
-
-        if (!startQuestion)
-        {
-            timeTillNextQuestion -= Time.deltaTime;
-        }
+       
         if (moveToNextLevel)
         {
             questionsAnswered = 0;
         }
-        if (questionsAnswered == 3)
-        {
-            spawnFadePrefab = true;
-        }
-        
-       
-        if (timeTillNextQuestion < 0 && currentQuestion == questionsAnswered)
-        {
-            currentQuestion++;
-            startQuestion = true;
-            timeTillNextQuestion = 15f;
-        }
-
-        if (fadeToEnd)
-        {
-//            AudioClip myClip = deathSound.clip;
-//            deathSound.PlayOneShot(myClip);
-        }
+ 
       
     }
     void AddPickup()
