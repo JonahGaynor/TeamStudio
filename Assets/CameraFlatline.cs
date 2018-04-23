9110e1a6150899b180c225b5d173e468b9e5289b
Unknown <jonahgaynor@gmail.com>
2018-04-23 15:13:43 -0400

    y tho

---
+++ Assets/CameraFlatline.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFlatline : MonoBehaviour {
+
+	AudioSource myAudio;
+	public AudioClip flatLine;
+
+	// Use this for initialization
+	void Start () {
+		myAudio = this.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ProofGameController.Instance.fadeToEnd) {
+			myAudio.PlayOneShot (flatLine, 0.05f);
+		}
+	}
+}
