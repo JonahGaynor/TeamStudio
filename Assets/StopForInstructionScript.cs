0e477b8c10332ea2dda8bb6f7f6d11366742e7cf
Unknown <sap565@nyu.edu>
2018-04-26 15:15:00 -0400

    Tons Of College scene Fixes

---
+++ Assets/StopForInstructionScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopForInstructionScript : MonoBehaviour {
+    public bool isStopped = false;
+    public float timeSinceStarting=0;
+    public Canvas instuctionCanvas;
+    public float timeAtWhichToStop = 2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        instuctionCanvas.enabled = false;
+        timeSinceStarting += Time.deltaTime;
+        Debug.Log(Time.timeScale);
+        Time.timeScale = 1;
+        if (timeSinceStarting >timeAtWhichToStop)
+        {
+            isStopped = true;
+            timeAtWhichToStop = 5000000000000000000000000000000f;
+        }
+        if (isStopped)
+        {
+            instuctionCanvas.enabled = true;
+            Time.timeScale = 0;
+                
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isStopped = false;
+        }
+    }
+}
